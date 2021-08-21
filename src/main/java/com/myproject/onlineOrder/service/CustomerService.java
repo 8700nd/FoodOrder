8cb7e42bd1e86845720b14f723e2a8c7cee53aef
8700nd <rossilyx@gmail.com>
2021-08-21 09:27:06 -0700

    initial

---
+++ src/main/java/com/myproject/onlineOrder/service/CustomerService.java
@@ -0,0 +1,26 @@
+package com.myproject.onlineOrder.service;
+
+import com.myproject.onlineOrder.dao.CustomerDao;
+import com.myproject.onlineOrder.entity.Cart;
+import com.myproject.onlineOrder.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+    @Autowired
+    private CustomerDao customerDao;
+
+    public void signUp(Customer customer) {
+        Cart cart = new Cart();
+        customer.setCart(cart);
+
+        customer.setEnabled(true);
+        customerDao.signUp(customer);
+    }
+
+    public Customer getCustomer(String email) {
+        return customerDao.getCustomer(email);
+    }
+}
---
+++ src/main/java/com/myproject/onlineOrder/service/MenuInfoService.java
@@ -0,0 +1,29 @@
+package com.myproject.onlineOrder.service;
+
+import com.myproject.onlineOrder.dao.MenuInfoDao;
+import com.myproject.onlineOrder.entity.MenuItem;
+import com.myproject.onlineOrder.entity.Restaurant;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MenuInfoService {
+
+    @Autowired
+    private MenuInfoDao menuInfoDao;
+
+
+    public List<Restaurant> getRestaurants() {
+        return menuInfoDao.getRestaurants();
+    }
+
+    public List<MenuItem> getAllMenuItem(int restaurantId) {
+        return menuInfoDao.getAllMenuItem(restaurantId);
+    }
+
+    public MenuItem getMenuItem(int id) {
+        return menuInfoDao.getMenuItem(id);
+    }
+}
